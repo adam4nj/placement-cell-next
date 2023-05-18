86a11ca6500a3fdca6a383b96c23170d1cb1c35b
adamj <adamjamesnellissery@gmail.com>
2023-05-18 18:51:52 +0530

    rename header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/Footer"
-import Header from "@/components/Nav"
+import  { MainNav } from "@/components/Nav"
 
 const ContactPage = () => {
   return (
     <>
-        <Header />
+        <MainNav />
         <div className="flex flex-grow w-full h-screen">
         <h1 className="text-2xl">Contact Us</h1>
         </div>
