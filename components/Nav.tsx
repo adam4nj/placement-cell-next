729ad015826d4e713bc378c476c3426d7c9fbf02
adamj <adamjamesnellissery@gmail.com>
2023-07-04 18:42:48 +0530

    mobile nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,65 +1,116 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
-
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 import SignInButton from "./SignInButton";
 
 export default function Navbar() {
+  const [state, setState] = useState(false);
+
+  const navigation = [
+    { title: "About", path: "/about" },
+    { title: "Jobs", path: "/jobs" },
+    { title: "Admin", path: "/admin" },
+  ];
+
+  useEffect(() => {
+    document.onclick = (e) => {
+      const target = e.target as HTMLElement;
+      if (!target.closest(".menu-btn")) setState(false);
+    };
+  }, []);
+
+  const Brand = () => (
+    <div className="flex items-center justify-between py-3 md:block">
+      <a href="/">
+        <Image
+          src="assets/logo-light.svg"
+          width={110}
+          height={50}
+          alt="Placement Cell Logo"
+        />
+      </a>
+      <div className="md:hidden">
+        <button
+          className="menu-btn text-slate-600 hover:text-gray-500"
+          onClick={() => setState(!state)}
+        >
+          {state ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+
   return (
-    <NavigationMenu className="container w-full rounded-2xl px-5 py-3 mx-auto justify-between">
-      <NavigationMenuList className="flex mx-auto space-x-6">
-        <div className="flex items-start justify-stretch">
-          <NavigationMenuItem className="px-5">
-            <Image
-              src="assets/logo-light.svg"
-              width={80}
-              height={20}
-              alt="Placement Cell Logo"
-            />
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/about" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/jobs" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Jobs
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/internships" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Internships
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+    <div className="relative">
+      <header>
+        <div className={`md:hidden ${state ? "mx-2 pb-5" : "hidden"}`}>
+          <Brand />
         </div>
-      </NavigationMenuList>
-      <NavigationMenuList className="flex end-5">
-        <NavigationMenuItem className="end-0">
-          <SignInButton />
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+        <nav
+          className={`pb-5 md:text-sm ${
+            state
+              ? "absolute z-20 top-0 inset-x-0 bg-slate-100 rounded-xl mx-2 mt-2 md:mx-0 md:mt-0 md:relative md:bg-transparent border"
+              : ""
+          }`}
+        >
+          <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+            <Brand />
+            <div
+              className={`flex-1 items-center mt-8 md:mt-0 md:flex ${
+                state ? "block" : "hidden"
+              } `}
+            >
+              <ul className="flex-1 justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+                {navigation.map((item, idx) => {
+                  return (
+                    <li
+                      key={idx}
+                      className="text-md text-slate-900 hover:text-slate-700"
+                    >
+                      <Link href={item.path} className="block">
+                        {item.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+                <li>
+                  <SignInButton />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
